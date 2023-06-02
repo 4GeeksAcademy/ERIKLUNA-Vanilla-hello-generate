5d8445394c63ba78ee8b5caf06b4379e9f32cfbe
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-06-02 20:09:50 +0000

    dominioTotal

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-let pronoun = ['the', 'our', 'your'];
-let adj = ['great', 'big', 'best', 'brave'];
-let noun = ['jogger', 'racoon', 'Juan', 'ball', 'cat'];
+let pronoun = ["the", "our", "your"];
+let adj = ["great", "big", "best", "brave"];
+let noun = ["jogger", "racoon", "Juan", "ball", "cat"];
 
-for (let i=0; i<pronoun.length;i++){
-    for (let j=0; j<adj.length; j++){
-        for( let k=0; k<noun.length; k++){
-            let dominio = `${pronoun[i]}${adj[j]}${noun[k]}`;
-            let extensiones = ['.com', '.net', '.org' , '.io'];
-            for (let l=0; l<extensiones.length; l++){
-                let dominioTotal = `${dominio}${extensiones[l]}`;
-                console.log (dominioTotal);
-            }
-        }
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      let dominio = `${pronoun[i]}${adj[j]}${noun[k]}`;
+      let extensiones = [".com", ".net", ".org", ".io"];
+      for (let l = 0; l < extensiones.length; l++) {
+        let dominioTotal = `${dominio}${extensiones[l]}`;
+        console.log(dominioTotal);
+      }
     }
+  }
 }
-
-
-
