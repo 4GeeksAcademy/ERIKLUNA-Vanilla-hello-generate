ca069723f5ad15a2f199148a287143f6d57f4f60
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-06-02 20:04:10 +0000

    dominio

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-var pronoun = ['the','our'];
-var adj = ['great', 'big' ];
-var noun = ['jogger','racoon'];
+let pronoun = ['the', 'our', 'your'];
+let adj = ['great', 'big', 'best', 'brave'];
+let noun = ['jogger', 'racoon', 'Juan', 'ball', 'cat'];
+
+for (let i=0; i<pronoun.length;i++){
+    for (let j=0; j<adj.length; j++){
+        for( let k=0; k<noun.length; k++){
+            let dominio = `${pronoun[i]}${adj[j]}${noun[k]}`;
+            let extensiones = ['.com', '.net', '.org' , '.io'];
+            for (let l=0; l<extensiones.length; l++){
+                let dominioTotal = `${dominio}${extensiones[l]}`;
+                console.log (dominioTotal);
+            }
+        }
+    }
+}
+
+
+
