5c80a93348afa8f2c412be345855c7f0f58f7ab8
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-08-17 02:24:32 +0000

    nuevo codigo

--- src/app.js
+++ src/app.js
@@ -7,6 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-  console.log("Hello Rigo from the console!");
+  let generarDominio = [];
+
+  for (let i of pronoun) {
+    for (let j of adj) {
+      for (let k of noun) {
+        let dominio = `${i}${j}${k}`;
+
+        for (let l of [".com", ".net", ".org"]) {
+          let dominioTotal = `${dominio}${l}`;
+
+          generarDominio.push(dominioTotal);
+        }
+      }
+    }
+  }
+
+  console.log(generarDominio);
 };
